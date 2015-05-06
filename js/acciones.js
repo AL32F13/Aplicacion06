82738b15ec3f3071ec78a03e7e4b3d1d3f1ec167
gaby <gaby_morales626@live.com>
2015-05-06 14:35:04 -0500

    dos
    wrfetumj

--- js/acciones.js
+++ js/acciones.js
@@ -1,19 +1,33 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-$('#btnvibrar').on('tap', function(){	
-  navigator.vibrate(1000);
-}); //tap a btnvibrar 
-$('#btnbeep').on('tap', function(){	
-  navigator.beep(1);
-$('#izquierda').on('swipe left' , 
-  function () {
-	  alert("barrio a la izquierda")
-$('#derecha').on('swipe right' , 
-  function () {
-	  alert("barrio a la derecha")
-});
-});
+	 $('#btnvibrar').on('tap',function(){
+		 navigator.vibrate(1000);
+	 });//tap a btnvibrar
+	 
+	 $('#btnbeep').on('tap',function(){
+		 navigator.notification.beep(1);
+	 });//tap a btnbeep
+	 
+	 
+	 $('#izquierda').on('swipeleft',function(){
+	 alert("barrio a la izquierda");
+     })//tap a izquierda
+	 
+	  $('#derecha').on('swiperight',function(){
+	 alert("barrio a la derecha");
+     })//tap a derecha
+	 
+	 document.addEventListener("pause", function(){
+     $('#listado').append("<p> se pauso </p>" );
+ });
+	
+	 document.addEventListener("resume", function(){
+     $('#listado').append("<p> se reinicio </p>" );
+}); 
+ 
+  $(window).on('orientationchange1',function(e){
+  $('#listado').append("<p> orientacion:" + e.orientation +  "</p>" );
 });
 });
 });
